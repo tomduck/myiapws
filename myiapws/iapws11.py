bc90c9c8e51facb6ed9b34d1be144faf128499c3
Thomas Duck <tomduck@tomduck.ca>
2016-11-08 17:44:45 -0400

    Renamed functions, wrote unit tests.

--- myiapws/iapws11.py
+++ myiapws/iapws11.py
@@ -1,6 +1,5 @@
-#! /usr/bin/env python3
-
 # iapws11.py: Library for IAPWS 2011 melting and sublimation curves of water
+
 # Copyright (C) 2014, 2016 Thomas J. Duck
 #
 # Thomas J. Duck <tomduck@tomduck.ca>
@@ -19,12 +18,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""IAPWS 2011 melting and sublimation curves for water.
+"""IAPWS 2011 melting and sublimation curves for ordinary water substance.
 
 Overview:
 
   This package implements the IAWPS 2011 formulas for the melting and
-  sublimation curves of water.
+  sublimation curves of ordinary water substance.
 
 References:
 
@@ -36,13 +35,18 @@ References:
       http://www.iapws.org/relguide/MeltSub2011.pdf
 """
 
-def pmelt_iceIh(T):
-    """Ice Ih melting pressure (MPa) for temperature (K)"""
+# pylint: disable=invalid-name
+
+import numpy
+
+def pmelt_ice_Ih(T):
+    """Ice Ih melting pressure (Pa) at temperature T (K).
+    251.165 <= T <= 273.16 K"""
 
     T = numpy.array(T)
-    if not numpy.logical_and(251.165<=T,T<=273.16).all():
+    if not numpy.logical_and(251.165 <= T, T <= 273.16).all():
         raise ValueError('251.165 <= T <= 273.16 K for ice Ih melting line')
-    
+
     a = [0.119539337e7, 0.808183159e5, 0.333826860e4]
     b = [0.300000e1, 0.257500e2, 0.103750e3]
 
@@ -50,15 +54,16 @@ def pmelt_iceIh(T):
     pstar = 611.657  # Pa
 
     theta = T/Tstar
-    
-    return (1. + sum(a[i]*(1.-theta**b[i]) for i in range(3))) * pstar / 1.e6
 
-    
-def pmelt_iceIII(T):
-    """Ice III melting pressure (MPa) for temperature (K)"""
+    return (1. + sum(a[i]*(1.-theta**b[i]) for i in range(3))) * pstar
+
+
+def pmelt_ice_III(T):
+    """Ice III melting pressure (Pa) at temperature T (K).
+    251.165 <= T <= 256.164"""
 
     T = numpy.array(T)
-    if not numpy.logical_and(251.165<=T,T<=256.164).all():
+    if not numpy.logical_and(251.165 <= T, T <= 256.164).all():
         raise ValueError('251.165 <= T <= 256.164 K for ice III melting line')
 
     Tstar = 251.165  # K
@@ -66,14 +71,15 @@ def pmelt_iceIII(T):
 
     theta = T/Tstar
 
-    return (1. - 0.299948*(1.-theta**60)) * pstar
+    return (1. - 0.299948*(1.-theta**60)) * pstar * 1.e6
 
 
-def pmelt_iceV(T):
-    """Ice V melting pressure (MPa) for temperature (K)"""
+def pmelt_ice_V(T):
+    """Ice V melting pressure (Pa) at temperature T (K).
+    256.164 <= T <= 273.31"""
 
     T = numpy.array(T)
-    if not numpy.logical_and(256.164<=T,T<=273.31).all():
+    if not numpy.logical_and(256.164 <= T, T <= 273.31).all():
         raise ValueError('256.164 <= T <= 273.31 for ice V melting line')
 
     Tstar = 256.164  # K
@@ -81,14 +87,15 @@ def pmelt_iceV(T):
 
     theta = T/Tstar
 
-    return (1. - 1.18721*(1.-theta**8)) * pstar
+    return (1. - 1.18721*(1.-theta**8)) * pstar * 1.e6
 
 
-def pmelt_iceVI(T):
-    """Returns the ice VI melting pressure (MPa) for temperature (K)"""
+def pmelt_ice_VI(T):
+    """Ice VI melting pressure (Pa) at temperature T (K).
+    273.31 <= T <= 355"""
 
     T = numpy.array(T)
-    if not numpy.logical_and(273.31<=T,T<=355).all():
+    if not numpy.logical_and(273.31 <= T, T <= 355).all():
         raise ValueError('273.31 <= T <= 355 for ice VI melting line')
 
     Tstar = 273.31  # K
@@ -96,14 +103,15 @@ def pmelt_iceVI(T):
 
     theta = T/Tstar
 
-    return (1. - 1.07476*(1.-theta**4.6)) * pstar
+    return (1. - 1.07476*(1.-theta**4.6)) * pstar * 1.e6
 
 
-def pmelt_iceVII(T):
-    """Ice VII melting pressure (MPa) for temperature (K)"""
+def pmelt_ice_VII(T):
+    """Ice VII melting pressure (Pa) at temperature T (K).
+    355 <= T <= 715"""
 
     T = numpy.array(T)
-    if not numpy.logical_and(355<=T,T<=715).all():
+    if not numpy.logical_and(355 <= T, T <= 715).all():
         raise ValueError('355 <= T <= 715 for ice VII melting line')
 
     Tstar = 355.0   # K
@@ -111,25 +119,25 @@ def pmelt_iceVII(T):
 
     theta = T/Tstar
 
-    return numpy.exp( (0.173683e1*(1.-1./theta)
-                        - 0.544606e-1*(1.-theta**5)
-                        + 0.806106e-7*(1.-theta**22)) ) * pstar
+    return numpy.exp((0.173683e1*(1.-1./theta)
+                      - 0.544606e-1*(1.-theta**5)
+                      + 0.806106e-7*(1.-theta**22))) * pstar * 1.e6
 
-    
-def psubl(T):
-    """Ice sublimation pressure (MPa) for temperature (K)"""
+
+def psubl_ice_Ih(T):
+    """Ice Ih sublimation pressure (Pa) at temperature T (K).
+    50 <= T <= 273.16 K"""
 
     T = numpy.array(T)
-    if not numpy.logical_and(50.<=T,T<=273.16).all():
+    if not numpy.logical_and(50. <= T, T <= 273.16).all():
         raise ValueError('50 <= T <= 273.16 K for ice sublimation line')
 
-    a = [-0.212144006e2,0.273203819e2,-0.610598130e1]
-    b = [0.333333333e-2,0.120666667e1,0.170333333e1]
+    a = [-0.212144006e2, 0.273203819e2, -0.610598130e1]
+    b = [0.333333333e-2, 0.120666667e1, 0.170333333e1]
 
     Tstar = 273.16   # K
     pstar = 611.657  # Pa
 
     theta = T/Tstar
-    
-    return numpy.exp(1./theta*sum(a[i]*theta**b[i] for i in range(3))) \
-      * pstar / 1.e6
+
+    return numpy.exp(1./theta*sum(a[i]*theta**b[i] for i in range(3))) * pstar
---
+++ test/test_iapws11.py
@@ -0,0 +1,87 @@
+#! /usr/bin/env python3
+
+# Copyright (C) 2016 Thomas J. Duck
+#
+# Thomas J. Duck <tomduck@tomduck.ca>
+# Department of Physics and Atmospheric Science, Dalhousie University
+# PO Box 15000 | Halifax NS B3H 4R2 Canada
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Unit tests for myiapws.iapws11 module."""
+
+# All test values are from Table 1 in [1].
+
+# References:
+#
+#   [1] Revised release on the pressure along the melting and sublimation
+#       curves of ordinary water substance.
+#
+#       IAPWS, 2011.
+#
+#       http://www.iapws.org/relguide/MeltSub2011.pdf
+
+# pylint: disable=invalid-name
+# pylint: disable=wildcard-import, unused-wildcard-import
+
+import unittest
+import sys
+
+import numpy
+
+from myiapws.iapws11 import *
+
+_VERBOSE = False  # Flag for unit test verbosity
+
+class Test_public(unittest.TestCase):
+    """Tests public API."""
+
+    def assert_close(self, v1, v2, rtol=1.e-8, atol=0.):
+        """Asserts scalars or arrays v1 and v2 are close."""
+        isclose = numpy.isclose(v1, v2, rtol=rtol, atol=atol)
+        if numpy.isscalar(isclose):
+            self.assert_(isclose)
+        else:
+            self.assert_(isclose.all())
+
+    def test_all(self):
+        """Test all of the functions."""
+        self.assert_close(pmelt_ice_Ih(260), 138.268e6, rtol=1.e-6)
+        self.assert_close(pmelt_ice_III(254), 268.685e6, rtol=1.e-5)
+        self.assert_close(pmelt_ice_V(265), 479.640e6, rtol=1.e-6)
+        self.assert_close(pmelt_ice_VI(320), 1356.76e6, rtol=1.e-5)
+        self.assert_close(pmelt_ice_VII(550), 6308.71e6, rtol=1.e-6)
+        self.assert_close(psubl_ice_Ih(230), 8.94735, rtol=1.e-6)
+
+
+def run_tests():
+    """Runs the unit tests."""
+    Nerrors = 0
+    Nfailures = 0
+
+    for Test in [Test_public]:
+        suite = unittest.makeSuite(Test)
+        result = unittest.TextTestRunner(verbosity=2 if _VERBOSE else 1)\
+          .run(suite)
+        Nerrors += len(result.errors)
+        Nfailures += len(result.failures)
+
+    if Nerrors or Nfailures:
+        print('\n\nSummary: %d errors and %d failures reported\n'%\
+            (Nerrors, Nfailures))
+
+    return Nerrors+Nfailures
+
+
+if __name__ == '__main__':
+    sys.exit(run_tests())
