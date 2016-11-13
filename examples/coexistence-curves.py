4387199d17eeeebcf033c34b75ac245892807f6b
Thomas Duck <tomduck@tomduck.ca>
2016-11-13 19:33:34 -0400

    Changed top-level docstring.

--- examples/coexistence-curves.py
+++ examples/coexistence-curves.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Pressure-temperature phase diagram for water."""
+"""Pressure-temperature coexistence curves for water."""
 
 # pylint: disable=invalid-name
 
